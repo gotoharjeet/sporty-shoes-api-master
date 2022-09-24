af2ce6bc672b5738d52b812efee1966fcdcfc3c5
Harjeet Singh <hskainth@itdc.co.in>
2022-09-24 17:32:51 +0530

    phase3-sporty-shoes-project

---
+++ src/main/java/com/sportyshoes/model/User.java
@@ -0,0 +1,123 @@
+package com.sportyshoes.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+
+	@Column(name = "name")
+	private String userName;
+
+	@Column(name = "email")
+	private String userEmail;
+
+	@Column(name = "password")
+	private String userPassword;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	@JoinTable(name = "USER_PRODUCT", joinColumns = @JoinColumn(name = "USER_ID"), inverseJoinColumns = @JoinColumn(name = "PRODUCT_ID"))
+	private List<Product> products = new ArrayList<Product>();
+
+	public User(String userName, String userEmail) {
+		this.userEmail = userEmail;
+		this.userName = userName;
+	}
+
+	
+	
+	public int getUserId() {
+		return userId;
+	}
+
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+
+
+	public void addProduct(Product product) {
+		this.products.add(product);
+	}
+	
+	@Override
+	public String toString() {
+		return "Custom ToString -> User [userId=" + userId + ", userName=" + userName + ", userEmail=" + userEmail + ", userPassword="
+				+ userPassword + ", products=" + products + "]";
+	}
+
+}
